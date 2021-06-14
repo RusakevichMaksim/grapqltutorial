7e4291169780ca1ca2576eb6d7491db4b4ce39f4
RusakevichMR <rusakevichmr@gmail.com>
2021-06-14 13:30:06 +0300

    qury in db

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -11,64 +11,6 @@ const {
 const Movies = require("../models/movie");
 const Directors = require("../models/director");
 
-// const movies = [
-//   {
-//     id: "1",
-//     name: "Pulp Fiction",
-//     genre: "Crime",
-//     directorId: "60c7221bfc1298b7dd4d25c9",
-//   },
-//   {
-//     id: "2",
-//     name: "1984",
-//     genre: "Sci-Fi",
-//     directorId: "60c72256fc1298b7dd4d25ca",
-//   },
-//   {
-//     id: "3",
-//     name: "V for vendetta",
-//     genre: "Sci-Fi-Triller",
-//     directorId: "60c722b5fc1298b7dd4d25cb",
-//   },
-//   {
-//     id: "4",
-//     name: "Snatch",
-//     genre: "Crime-Comedy",
-//     directorId: "60c722ecfc1298b7dd4d25cd",
-//   },
-//   {
-//     id: "5",
-//     name: "Reservoir Dogs",
-//     genre: "Crime",
-//     directorId: "60c7221bfc1298b7dd4d25c9",
-//   },
-//   {
-//     id: "6",
-//     name: "The Hateful Eight",
-//     genre: "Crime",
-//     directorId: "60c7221bfc1298b7dd4d25c9",
-//   },
-//   {
-//     id: "7",
-//     name: "Inglourious Basterds",
-//     genre: "Crime",
-//     directorId: "60c7221bfc1298b7dd4d25c9",
-//   },
-//   {
-//     id: "7",
-//     name: "Lock, Stock and Two Smoking Barrels",
-//     genre: "Crime-Comedy",
-//     directorId: "60c722ecfc1298b7dd4d25cd",
-//   },
-// ];
-
-// const directors = [
-//   { id: "1", name: "Quentin Tarantino", age: 55 }, //60c7221bfc1298b7dd4d25c9
-//   { id: "2", name: "Michael Radford", age: 72 }, //60c72256fc1298b7dd4d25ca
-//   { id: "3", name: "James McTeigue", age: 51 }, //60c722b5fc1298b7dd4d25cb
-//   { id: "4", name: "Guy Ritchie", age: 50 }, //60c722ecfc1298b7dd4d25cd
-// ];
-
 const MovieType = new GraphQLObjectType({
   name: "Movie",
   fields: () => ({
@@ -78,7 +20,7 @@ const MovieType = new GraphQLObjectType({
     director: {
       type: DirectorType,
       resolve(parent, args) {
-        return directors.find((director) => director.id === parent.id);
+        return Directors.findById(parent.directorId);
       },
     },
   }),
@@ -93,7 +35,7 @@ const DirectorType = new GraphQLObjectType({
     movies: {
       type: new GraphQLList(MovieType),
       resolve(parent, args) {
-        return movies.filter((movie) => movie.directorId === parent.id);
+        return Movies.find({ directorId: parent.id });
       },
     },
   }),
@@ -106,26 +48,26 @@ const Query = new GraphQLObjectType({
       type: MovieType,
       args: { id: { type: GraphQLID } },
       resolve(parent, args) {
-        return movies.find((movie) => movie.id === args.id);
+        return Movies.findById(args.id);
       },
     },
     director: {
       type: DirectorType,
       args: { id: { type: GraphQLID } },
       resolve(parent, args) {
-        return directors.find((director) => director.id === args.id);
+        return Directors.findById(args.id);
       },
     },
     movies: {
       type: new GraphQLList(MovieType),
       resolve(parent, args) {
-        return movies;
+        return Movies.find();
       },
     },
     directors: {
       type: new GraphQLList(DirectorType),
       resolve(parent, args) {
-        return directors;
+        return Directors.find();
       },
     },
   },
---
+++ test.js
@@ -0,0 +1,3 @@
+let movie = "60c723aafc1298b7dd4d25d0";
+
+let dir = "60c7221bfc1298b7dd4d25c9";
